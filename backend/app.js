1bfddd091a66d3f3c368b702978c7c5849042c47
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-26 22:53:23 -0700

    express server security

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const logger = require('morgan');
 const csurf = require('csurf');
 const cors = require('cors');
 const { isProduction } = require('./config/keys.js');
+const debug = require('debug');
 
 const usersRouter = require('./routes/api/users');
 const tweetsRouter = require('./routes/api/tweets');
@@ -31,5 +32,25 @@ app.use(
 app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    
+    res.json({
+        message: err.message,
+        statusCode,
+        errors: err.errors
+    })
+});
 
 module.exports = app;
