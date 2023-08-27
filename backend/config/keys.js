5ba4fb77e9bcc0cdd83bc725d5fbe57f046a24a1
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-27 15:56:06 -0700

    JWT

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,7 @@
+console.log('Environment variable SECRET_OR_KEY:', process.env.SECRET_OR_KEY);
+
 module.exports = {
+    secretOrKey: process.env.SECRET_OR_KEY,
     mongoURI: process.env.MONGO_URI,
     isProduction: process.env.NODE_ENV === 'production'
-}
+};
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,11 +4,28 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const passport = require('passport');
+const { loginUser, restoreUser } = require('../../config/passport');
+const { isProduction } = require('../../config/keys');
 
 router.get('/', function(req, res, next) {
     res.json({
         message: "GET /api/users"
     })
+});
+
+router.get('/current', restoreUser, (req, res) => {
+    if (!isProduction) {
+        const csrfToken = req.csrfToken();
+        res.cookie("CSRF-TOKEN", csrfToken);
+    }
+
+    if (!req.user) return res.json(null);
+
+    res.json({
+        _id: req.user._id,
+        username: req.user.username,
+        email: req.user.email
+    });
 });
 
 router.post('/register', async (req, res, next) => {
@@ -47,7 +64,7 @@ router.post('/register', async (req, res, next) => {
             try {
                 newUser.hashedPassword = hashedPassword;
                 const user = await newUser.save();
-                return res.json({ user });
+                return res.json(await loginUser(user));
             } catch(err) {
                 next(err);
             }
@@ -58,14 +75,14 @@ router.post('/register', async (req, res, next) => {
 router.post('/login', async (req, res, next) => {
     passport.authenticate('local', async function(err, user) {
         if (err) return next(err);
-        
+
         if (!user) {
             const err = new Error('Invalid credentials');
             err.statusCode = 400;
             err.errors = { email: "Invalid credentials" };
             return next(err);
         }
-        return res.json({ user });
+        return res.json(await loginUser(user));
     })(req, res, next);
 });
 module.exports = router;
