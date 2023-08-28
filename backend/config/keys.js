9c1939eb6c2a7472583406c83a7d06fbf958d9e7
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-27 18:35:59 -0700

    validations

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,5 +1,3 @@
-console.log('Environment variable SECRET_OR_KEY:', process.env.SECRET_OR_KEY);
-
 module.exports = {
     secretOrKey: process.env.SECRET_OR_KEY,
     mongoURI: process.env.MONGO_URI,
