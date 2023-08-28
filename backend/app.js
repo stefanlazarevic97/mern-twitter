3eb5950316e7e22154725d696aec4e60a6ceb032
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-27 21:46:10 -0700

    ready for render hosting

--- backend/app.js
+++ backend/app.js
@@ -39,6 +39,28 @@ app.use('/api/users', usersRouter);
 app.use('/api/tweets', tweetsRouter);
 app.use('/api/csrf', csrfRouter);
 
+if (isProduction) {
+    const path = require('path');
+
+    app.get('/', (req, res) => {
+        res.cookie('CSRF-TOKEN', req.csrfToken());
+        
+        res.sendFile(
+            path.resolve(__dirname, '../frontend', 'build', 'index.html')
+        );
+    });
+
+    app.use(express.static(path.resolve("../frontend/build")));
+
+    app.get(/^(?!\/?api).*/, (req, res) => {
+        res.cookie('CSRF-TOKEN', req.csrfToken());
+        
+        res.sendFile(
+            path.resolve(__dirname, '../frontend', 'build', 'index.html')
+        );
+    });
+}
+
 app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.statusCode = 404;
