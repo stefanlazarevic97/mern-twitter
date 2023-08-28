98696b85898d96a5a34cb7b322da39c1225823da
stefanlazarevic <stefanlaza97@gmail.com>
2023-08-27 21:26:05 -0700

    add redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import { Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
 import NavBar from './components/NavBar/NavBar';
-
 import MainPage from './components/MainPage/MainPage';
 import LoginForm from './components/SessionForms/LoginForm';
 import SignupForm from './components/SessionForms/SignupForm';
